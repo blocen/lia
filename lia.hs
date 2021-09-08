dbc9539a64fe113c45bcf5ca736d195799347f52
Raphael <89421168+blocen@users.noreply.github.com>
2021-09-08 09:42:48 +0200

    chainLength

--- lia.hs
+++ lia.hs
@@ -1,11 +1,21 @@
+import Text.ParserCombinators.Parsec.Combinator (chainl)
+
+-- 2 data constructors
 data Chain
   = GenesisBlock
   | Block Chain Txs
 
 type Txs = Int
 
+chain1 :: Chain
 chain1 =
   Block GenesisBlock 2
 
+chain2 :: Chain
 chain2 =
   Block chain1 4
+
+-- type signature
+chainLength :: Chain -> Int
+chainLength GenesisBlock = 0
+chainLength (Block c _) = chainLength c + 1
