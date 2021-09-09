531aa5a015886183c7955ee36af1fbf4e7626dc0
Raphael <89421168+blocen@users.noreply.github.com>
2021-09-09 08:14:01 +0200

    Hello input

---
+++ uno.hs
@@ -0,0 +1,9 @@
+main = putStrLn "Hello, World!"
+
+-- do line <- getLine; putStrLn line
+
+-- map length ["abc", "abcdef"]
+-- filter (\x -> length x > 1) ["abc", "d", "ef"]
+-- map (* 3) [1, 2, 3]
+-- primes = [n | n <- [2 ..], all (\k -> n `mod` k /= 0) [2 .. n `div` 2]]
+-- take 10 primes
