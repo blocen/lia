16842a725cc37683a29cf2666510736d9c673119
Raphael <89421168+blocen@users.noreply.github.com>
2021-09-10 22:58:54 +0200

    fact, fib

--- Gold.hs
+++ Gold.hs
@@ -30,3 +30,22 @@ greet "Finland" name = "Hei, " ++ name
 greet "Italy" name = "Ciao, " ++ name
 greet "England" name = "How do you do, " ++ name
 greet _ name = "Hello, " ++ name
+
+describe :: Integer -> String
+describe 0 = "zero"
+describe 1 = "one"
+describe 2 = "an even prime"
+describe n = "the number " ++ show n
+
+-- fact n
+fact :: Int -> Int
+fact n = if n <= 1 then 1 else n * fact (n - 1)
+
+-- fact 1 = 1
+-- fact n = n * fact (n-1)
+
+-- fib n
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 2
+fib n = fib (n - 2) + fib (n - 1)
