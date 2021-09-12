8a0ba4f361175166190fdf8a8959f0b316b64cd9
Raphael <89421168+blocen@users.noreply.github.com>
2021-09-12 09:24:57 +0200

    Bool; not, ||, ifthenelse

--- Data_types.hs
+++ Data_types.hs
@@ -14,14 +14,28 @@ comment
 -- False :: Bool
 -- True  :: Bool
 
--- functions:
--- fun :: Bool -> ...
--- fun False = ...
--- fun True  = ...
-
 not :: Bool -> Bool
 not False = True
 not True  = False
+
+(||) :: Bool -> Bool -> Bool
+(||) True _  = True
+(||) False b = b
+
+ifthenelse :: Bool -> a -> a -> a
+ifthenelse False _t e = e
+ifthenelse True t _e  = t
+
+-- would work if used with build-in Bool
+-- ifthenelse c t e = 
+--   if c
+--     then t
+--     else e
+
+-- guards
+-- ifthenelse c t e 
+--   | c         = t
+--   | otherwise = e
 
 --
 
