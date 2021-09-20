9d97f4eacc8953dd6c489f1797ec56acad14ac6e
Raphael <89421168+blocen@users.noreply.github.com>
2021-09-20 22:55:39 +0200

    Eq class definition; instance declaration

--- equality_class.hs
+++ equality_class.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
+
+
 -- Eq: equality class (constraint)
 -- additional constraint for what can be a
 
@@ -5,7 +8,7 @@
 -- (==) :: Eq a => a -> a -> Bool
 
 hasBlock :: Eq txs => txs -> Chain txs -> Bool
-hasBlock x GenesisBlock = False
+hasBlock _ GenesisBlock = False
 hasBlock x (Block c t) = 
   x == t || hasBlock x c
 
@@ -47,4 +50,36 @@ chain2 =
   Block chain1 4
 
 
+-- t: 0
+-- Num class
 
+-- class declaration:
+-- class: keyword / name/ a 'is a member of xy class' / where: keyword
+-- class Ep a where
+--   (==) :: a -> a -> Bool -- type signatures
+--   (/=) :: a -> a -> Bool
+--   {-# MINIMAL (==) | (/=) #-} -- pragma; either on, other is inferred
+
+-- class definition
+class MyEq a where
+  (===) :: a -> a -> Bool -- base operation
+
+(=/=) :: MyEq a => a -> a -> Bool
+(=/=) x y = not (x === y)
+
+-- how to make instance of a class?
+-- instance declaration
+instance MyEq Bool where
+  (===) :: Bool -> Bool -> Bool -- only with lang ext InstanceSigs!
+  (===) True True   = True
+  (===) False False = True
+  (===) _    _      = False
+
+instance MyEq a => MyEq [a] where
+  (===) :: MyEq a => [a] -> [a] -> Bool
+  (===) [] []            = True
+  (===) [] (y : ys)      = False
+  (===) (x : xs) []      = False
+  (===) (x: xs) (y : ys) = 
+    x === y && xs === ys
+  
