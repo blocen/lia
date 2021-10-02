8a2a1f21be6ddd4ea8acc11e4db254fdafd14dcf
Raphael <89421168+blocen@users.noreply.github.com>
2021-10-02 23:57:25 +0200

    Hangman guesses

--- tuts/Hangman.hs
+++ tuts/Hangman.hs
@@ -3,6 +3,7 @@ module Hangman where
 import System.IO
 import System.Random
 
+
 main :: IO ()
 main = do
   -- Computation 'openFile' @file mode@ allocates and returns a new, open
@@ -14,10 +15,25 @@ main = do
 
   gen <- getStdGen
   let words = map init (lines contents)
-      (n, _) = randomR (0, (length words) - 1) gen :: (Int, StdGen)
-  play (words !! n)
+      (n, _) = randomR (0, length words - 1) gen :: (Int, StdGen)
+      word = words !! n
+      -- putStrLn "n: " 
+
+  play word (map (\x -> '_') word)
   -- putStrLn ("there are " ++ show ( length (lines contents)) ++ " words.")
   hClose handle
 
-play word = do
-  putStrLn ("The word is " ++ word ++ ".")
+play :: [Char] -> [Char] -> IO ()
+play word known = do
+  -- putStrLn ("The word is " ++ word ++ ".")
+  putStrLn known
+  putStrLn "Enter a letter to guess: "
+  line <- getLine
+  play word (handle (head line) word known) -- head NOT for empty lists
+
+-- regular pure haskell function!
+handle :: Char -> [Char] -> [Char] -> [Char]
+handle letter word known
+  | letter `elem` word = zipWith (\w k -> if w == letter then w else k) word known
+  | otherwise          = known
+
